dc42f5f0a733916bf032ce4cd12201dbe6ba5c03
kennytm <kennytm@gmail.com>
2017-09-12 18:24:00 +0800

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,22 @@
-/**
+#![feature(libc)]
 
-```
-use std::os::raw::{c_char, c_int};
-#[cfg_attr(all(windows, target_env="msvc"), link(name="legacy_stdio_definitions", kind="static"))]
-extern "C" {
-    fn printf(_: *const c_char, ...) -> c_int;
+extern crate libc;
+
+// Ref: http://hermanradtke.com/2015/01/12/terminal-window-size-with-rust-ffi.html
+
+#[cfg(unix)]
+use libc::{STDOUT_FILENO, TIOCGWINSZ, ioctl, winsize};
+
+pub fn column_width() -> u16 {
+    #[cfg(unix)]
+    unsafe {
+        let mut w = winsize { ws_row: 0, ws_col: 0, ws_xpixel: 0, ws_ypixel: 0 };
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &mut w);
+        w.ws_col
+    }
 }
-unsafe {
-    use std::ffi::CString;
-    let fmt = CString::new("test\n").unwrap();
-    printf(fmt.as_ptr());
-    let fmt = CString::new("number = %d\n").unwrap();
-    printf(fmt.as_ptr(), 3);
-    let fmt = CString::new("%d, %d\n").unwrap();
-    printf(fmt.as_ptr(), 10, 5);
-}
-```
-
-*/
-
-pub fn wut() {}
 
 #[test]
-fn orly() {
-    use std::os::raw::{c_char, c_int};
-    #[cfg_attr(all(windows, target_env="msvc"), link(name="legacy_stdio_definitions", kind="static"))]
-    extern "C" {
-        fn printf(_: *const c_char, ...) -> c_int;
-    }
-    unsafe {
-        use std::ffi::CString;
-        let fmt = CString::new("test\n").unwrap();
-        printf(fmt.as_ptr());
-        let fmt = CString::new("number = %d\n").unwrap();
-        printf(fmt.as_ptr(), 3);
-        let fmt = CString::new("%d, %d\n").unwrap();
-        printf(fmt.as_ptr(), 10, 5);
-    }
+fn fetch_column_width() {
+    assert_eq!(column_width(), 235);
 }
