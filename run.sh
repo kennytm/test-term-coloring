fe46555db5141562485032146f4a575e9ce10d60
kennytm <kennytm@gmail.com>
2017-09-16 18:20:56 +0800

    test travis build stages

--- run.sh
+++ run.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-export PATH=$HOME/.cargo/bin:$HOME/.local/bin:$PATH
-
-cd /x
-cargo clean --color always
-cargo run --color always
-
-export TERM=xterm
-cargo clean --color always
-cargo run --color always
-
+echo $SOMETHING
