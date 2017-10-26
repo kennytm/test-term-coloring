1bbb45979f356dc64b6a3f6f3b8b0e70617cae17
kennytm <kennytm@gmail.com>
2017-10-27 06:18:58 +0800

    ?

--- run.sh
+++ run.sh
@@ -2,28 +2,6 @@
 
 set -ex
 
-SRC=$(pwd)
-
-cp config.toml ~/rust/
-cd ~/rust
-ls -a
-
-if [ ! -d ./dot-git ]; then
-    cd ..
-    git clone https://github.com/rust-lang/rust.git
-    cd rust
-else
-    mv dot-git .git
-    git fetch origin master
-    git reset --hard origin/master
-fi
-git remote add self https://github.com/kennytm/rust.git || true
-git fetch self travis-color-color-conflict
-git reset --hard self/travis-color-color-conflict
-git submodule update --recursive --init
-mv .git dot-git
-
-export PATH=$PATH:/home/travis/cmake-3.9.4-Linux-x86_64/bin
-cmake /V
-
-./x.py build --stage 0 src/librustc -j31
+travis_fold start foo
+echo ha
+travis_fold end foo
